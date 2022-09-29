6224c94cb2f1dbc65bce8139d9aacfd171457344
dhanaraj <dhanraj@propelsoft.in>
2022-09-29 15:39:45 +0530

    Addded Db files

--- database/mariodb29.sql
+++ database/mariodb29.sql
@@ -48626,6 +48626,7 @@ DROP TABLE IF EXISTS `order_details`;
 
 CREATE TABLE `order_details` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL DEFAULT 1,
   `company_name` varchar(50) NOT NULL,
   `gst` varchar(50) NOT NULL,
   `mobile` varchar(50) NOT NULL,
@@ -48637,14 +48638,32 @@ CREATE TABLE `order_details` (
   `approval_status` int(11) NOT NULL DEFAULT 1,
   `transaction_status` int(11) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `order_details` */
 
-insert  into `order_details`(`id`,`company_name`,`gst`,`mobile`,`dist_id`,`town`,`address`,`quantity`,`date`,`approval_status`,`transaction_status`) values 
-(1,'tcc','33ASKPK0273B4Zr','8056259119',3631,'Trichy','trichy',5,'2022-09-29',1,0),
-(2,'metajob','33ASKPK0273B4Ze','8610785311',3635,'Trichy','trichy',3,'2022-09-29',1,0),
-(3,'jp','33ASKPK0273B4Ze','9443447755',306,'Trichy','trichy',8,'2022-09-29',1,0);
+insert  into `order_details`(`id`,`product_id`,`company_name`,`gst`,`mobile`,`dist_id`,`town`,`address`,`quantity`,`date`,`approval_status`,`transaction_status`) values 
+(1,1,'tcc','33ASKPK0273B4Zr','8056259119',3631,'Trichy','trichy',5,'2022-09-29',1,0),
+(2,1,'metajob','33ASKPK0273B4Ze','8610785311',3635,'Trichy','trichy',3,'2022-09-29',1,0),
+(3,2,'jp','33ASKPK0273B4Ze','9443447755',306,'Trichy','trichy',8,'2022-09-29',1,0),
+(4,1,'Trichy Car Care','33ASKPK0273B4Zr','8056259119',4147,'keeranur','south street,ayyanar kovil Street',4,'2022-09-29',1,0),
+(5,1,'Harish','33ASKPK0273B4Zr','9442769739',3714,'perungudi','North Keela street,west side',4,'2022-09-29',1,0);
+
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+/*Data for the table `products` */
+
+insert  into `products`(`id`,`name`) values 
+(1,'Pen'),
+(2,'Pencil');
 
 /*Table structure for table `states` */
 
