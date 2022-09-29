cb865480de80da851a7c8d33d1a497bc7bd883e8
pandiyarajan-propel <pandiyarajan@propelsoft.in>
2022-09-29 12:41:58 +0530

    add header

--- admin/layout/header.php
+++ admin/layout/header.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mario Precision</title>
-    <link rel="stylesheet" href="http://localhost/php/mario/assets/admincss/index.css">
-    <script src="http://localhost/php/mario/assets/jquery/jquery.min.js"></script>
+    <link rel="stylesheet" href="http://localhost/php/mario_precission_automotive/assets/admincss/index.css">
+    <script src="http://localhost/php/mario_precission_automotive/assets/jquery/jquery.min.js"></script>
 </head>
 <body>
     
@@ -31,10 +31,16 @@
 <section class="side-bar">
   
 <ul class="menu">
-   <a href="http://localhost/mario"> <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+   <a href="http://localhost/php/mario_precission_automotive/admin/home.php"> <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
   <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
 </svg>Home</li></a>
+<a href="http://localhost/php/mario_precission_automotive/admin/home1.php"> <li><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293l6-6zm5-.793V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+  <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+</svg>Add Product</li></a>
+
+
 </ul>
 </section>
 <div class="main-container">
