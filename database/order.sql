fbbe9dc15815bd9ca4df75d7660a1b7b4b0ef4a0
dhanaraj <dhanraj@propelsoft.in>
2022-10-01 14:21:08 +0530

    added tables

--- database/order.sql
+++ database/order.sql
@@ -1,21 +1,39 @@
 /*
 SQLyog Community v13.1.7 (64 bit)
-MySQL - 10.4.22-MariaDB 
+MySQL - 10.4.22-MariaDB : Database - mario
 *********************************************************************
 */
+
 /*!40101 SET NAMES utf8 */;
 
-create table `order_details` (
-	`id` int (11),
-	`product_id` int (11),
-	`company_name` varchar (150),
-	`gst` varchar (150),
-	`mobile` varchar (150),
-	`dist_id` int (11),
-	`town` varchar (150),
-	`address` varchar (150),
-	`quantity` int (11),
-	`date` date ,
-	`approval_status` int (11),
-	`transaction_status` int (11)
-); 
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `order_details` */
+
+DROP TABLE IF EXISTS `order_details`;
+
+CREATE TABLE `order_details` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL DEFAULT 1,
+  `company_name` varchar(50) NOT NULL,
+  `gst` varchar(50) NOT NULL,
+  `mobile` varchar(50) NOT NULL,
+  `dist_id` int(11) NOT NULL,
+  `town` varchar(50) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `approval_status` int(11) NOT NULL DEFAULT 0,
+  `transaction_status` int(11) NOT NULL DEFAULT 0,
+  `delivery_status` int(11) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
--- database/product.sql
+++ database/product.sql
@@ -1,20 +1,38 @@
 /*
 SQLyog Community v13.1.7 (64 bit)
-MySQL - 10.4.22-MariaDB 
+MySQL - 10.4.22-MariaDB : Database - mario
 *********************************************************************
 */
+
 /*!40101 SET NAMES utf8 */;
 
-create table `products` (
-	`id` int (11),
-	`date` date ,
-	`name` varchar (300),
-	`mrp` int (11),
-	`quantity` int (11),
-	`sprice` int (11),
-	`description` text ,
-	`youtube_link` varchar (300),
-	`image_1` varchar (300),
-	`image_2` varchar (300),
-	`image_3` varchar (300)
-); 
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `name` varchar(100) NOT NULL,
+  `mrp` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `sprice` int(11) NOT NULL,
+  `description` text DEFAULT NULL,
+  `youtube_link` varchar(100) DEFAULT NULL,
+  `image_1` varchar(100) DEFAULT NULL,
+  `image_2` varchar(100) DEFAULT NULL,
+  `image_3` varchar(100) DEFAULT NULL,
+  `active_status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
