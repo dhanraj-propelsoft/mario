bfa6c5c16f3c6ee898ffa540fb2ef0776371a04c
VallatharasuPropelSoft <vallatharasu@propelsoft.in>
2022-10-03 11:57:48 +0530

    some issue solved

--- index.php
+++ index.php
@@ -51,13 +51,17 @@ $productList = mysqli_query($con, "select*from products WHERE active_status='1' 
 
 </section>
 <div class="splash-screen">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+  <path fill="#ff5500" fill-opacity="0.2" d="M0,288L48,272C96,256,192,224,288,213.3C384,203,480,213,576,218.7C672,224,768,224,864,202.7C960,181,1056,139,1152,117.3C1248,96,1344,96,1392,96L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+</svg>
+</svg>
+
     <div class="splash-content">
         <p class="splash-deal">Today's Deal <br> From</p>
         <h1 class="brand-name"><span>M</span>ario <span>P</span>recision <span>A</span>utomotive</h1>
         <p>Powered by propel soft</p>
     </div>
 
-</div>
 <script>
     var a = $("iframe");
   for (let i = 0; i< a.length; i++) {
